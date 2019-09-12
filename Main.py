a19c124f804fbc556a9652dddc3784a1ef079df0
Carrie-West <carrie105violet@gmail.com>
2019-09-12 14:47:35 -0400

    Added Other Pronoun Options

--- Main.py
+++ Main.py
@@ -6,18 +6,24 @@ heHim=["he", "him", "his"]
 sheHer=["she", "her", "her's"]
 theyThem=["they", "them', their's"]
 otherPronouns=[]
+acceptedPronouns="N"
 pronounSet=input("What pronouns do you use? (He,She,They,Other)")
-if pronounSet == "he":
+if pronounSet == "He":
+    pronounSet = heHim
 elif pronounSet == "She":
+    pronounSet = sheHer
 elif pronounSet == "They":
-elif prnounsSet == "Other":
-    otherPronounsOne=input("Valid! What should others use to refer to you? (ex: 'he' or 'she' ")
-    otherPronouns.append(otherPronounsOne)
-    otherPronounsTwo = input("What should others use to refer to you? (ex: 'her' or 'them')")
-    otherPronouns.append(otherPronounsTwo)
-    otherPronounsThree = input("What should others use to refer to your possessions? (ex: 'their's' or 'his')")
-    otherPronouns.append(otherPronounsThree)
-    print(otherPronouns)
-    acceptedPronouns=input("Are these pronouns correct? (Y/N)")
-    if acceptedPronouns = Y:
-        break
+    pronounSet = theyThem
+elif pronounSet == "Other":
+    print("Awesome! You're valid.")
+    while acceptedPronouns != "Y":
+        otherPronounsOne=input("What should others use to refer to you? (ex: 'he' or 'she') ")
+        otherPronouns.append(otherPronounsOne)
+        otherPronounsTwo = input("What should others use to refer to you? (ex: 'her' or 'them')")
+        otherPronouns.append(otherPronounsTwo)
+        otherPronounsThree = input("What should others use to refer to your possessions? (ex: 'their's' or 'his')")
+        otherPronouns.append(otherPronounsThree)
+        print(otherPronouns)
+        acceptedPronouns=input("Are these pronouns correct? (Y/N)")
+        pronounSet=otherPronouns
+print(pronounSet)
